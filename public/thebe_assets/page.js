7b218d3a2850fd2eb96a7591ce606ac7c352af82
Fred Chasen <fchasen@gmail.com>
2015-06-29 13:59:32 -0400

    remove public content

--- public/thebe_assets/page.js
+++
@@ -1,9 +0,0 @@
-var thebe;
-$(function(){
-    thebe = new Thebe({
-      url:"http://192.168.59.103:8888/",
-      tmpnb_mode: false,
-      append_kernel_controls_to: 'body'
-    });
-    $('section[data-type=chapter]').addClass('page');
-});
